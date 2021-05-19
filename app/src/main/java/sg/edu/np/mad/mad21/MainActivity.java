d44c7d9de93318fad13abec2b259c7058864a14b
westwq <twq2@np.edu.sg>
2021-05-19 16:53:10 +0800

    store pref n skip scene

--- app/src/main/java/sg/edu/np/mad/mad21/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad21/MainActivity.java
@@ -3,28 +3,56 @@ package sg.edu.np.mad.mad21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final static String SKIP = "pref_skip";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new CountDownTimer(3000, 3000) {
+        SharedPreferences pref = getSharedPreferences(SKIP, MODE_PRIVATE);
+        if(pref.getBoolean(SKIP, false)) {
+            launchRV();
+        }
+        else {
 
-            @Override
-            public void onTick(long l) {
+            CheckBox cb = findViewById(R.id.checkBox);
+            cb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SharedPreferences.Editor prefEdit = getSharedPreferences(SKIP, MODE_PRIVATE).edit();
+                    prefEdit.putBoolean(SKIP, true);
+                    prefEdit.apply();
 
-            }
+                    launchRV();
+                }
+            });
 
-            @Override
-            public void onFinish() {
-                Intent i = new Intent(MainActivity.this, ListActivity.class);
-                startActivity(i);
-            }
-        }.start();
+            //Countdown can be in oncreate/../onresume
+            new CountDownTimer(3000, 3000) {
+
+                @Override
+                public void onTick(long l) {
+
+                }
+
+                @Override
+                public void onFinish() {
+                    launchRV();
+                }
+            }.start();
+        }
+    }
+
+    private void launchRV()
+    {
+        Intent i = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(i);
     }
 }
