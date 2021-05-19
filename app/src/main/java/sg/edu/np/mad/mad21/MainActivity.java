c9aa55273dfa8ff919b4a87c30cae29e6e0dda3b
westwq <twq2@np.edu.sg>
2021-05-19 16:33:52 +0800

    countdown

--- app/src/main/java/sg/edu/np/mad/mad21/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad21/MainActivity.java
@@ -2,7 +2,9 @@ package sg.edu.np.mad.mad21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new CountDownTimer(3000, 3000) {
+
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent i = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(i);
+            }
+        }.start();
     }
 }
